4aa11f3c6cb1759c8e2a8c912e13531b9fe46832
ahmedmourouj <ahmedrebai2001@gmail.com>
2023-09-07 13:56:38 +0100

    first commit

---
+++ src/components/LineChart.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Line } from "react-chartjs-2";
+import { Col, Row, Typography } from "antd";
+
+
+
+const { Title } = Typography;
+
+const LineChart = ({  currentPrice, coinName, change, time }) => {
+ 
+
+  
+
+  return (
+    <>
+      <Row className="chart-header">
+        <Title level={2} className="chart-title">
+          {coinName} Price Chart
+        </Title>
+        <Col className="price-container">
+          <Title level={5} className="price-change">
+            Change: {change}%
+          </Title>
+          <Title level={5} className="current-price">
+            Current {coinName} Price: $ {currentPrice}
+          </Title>
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+export default LineChart;
---
+++ src/services/cryptoNewsApi.js
@@ -0,0 +1,26 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const cryptoNewsHeaders = {
+  "X-BingApis-SDK": "true",
+  "X-RapidAPI-Key": "ea0f15634dmshc4fafc6e41721bcp1e7879jsn95c519477e26",
+  "X-RapidAPI-Host": "bing-news-search1.p.rapidapi.com",
+};
+
+const baseUrl = "https://bing-news-search1.p.rapidapi.com";
+
+const createRequest = (url) => ({ url, headers: cryptoNewsHeaders });
+
+export const cryptoNewsApi = createApi({
+  reducerPath: "cryptoNewsApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptoNews: builder.query({
+      query: ({ newsCategory, count }) =>
+        createRequest(
+          `/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Week&count=${count}`
+        ),
+    }),
+  }),
+});
+
+export const { useGetCryptoNewsQuery } = cryptoNewsApi;
